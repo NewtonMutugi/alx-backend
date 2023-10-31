a43c832511e76baeadd6238613568cfae455ebb8
NewtonMutugi <newtonmut197@gmail.com>
2023-10-31 17:50:33 +0300

    Added 1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config class for the application."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index() -> str:
+    """Renders the index.html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
