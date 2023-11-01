9a89f6ab9c8beb25e4940ed3ea95720878348a15
NewtonMutugi <newtonmut197@gmail.com>
2023-11-01 22:21:48 +0300

    Added 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" 2-lifo_cache """
+""" 3-lru_cache """
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """LifoCache dictionary"""
+class LRUCache(BaseCaching):
+    """LruCache dictionary"""
 
     def __init__(self):
         """Constructor"""
@@ -17,7 +17,7 @@ class LIFOCache(BaseCaching):
             if key in self.cache_data:
                 self.queue.remove(key)
             elif len(self.cache_data) >= self.MAX_ITEMS:
-                discard = self.queue.pop()
+                discard = self.queue.pop(0)
                 del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
             self.queue.append(key)
@@ -27,4 +27,6 @@ class LIFOCache(BaseCaching):
         """Get an item by key"""
         if key is None or key not in self.cache_data.keys():
             return None
+        self.queue.remove(key)
+        self.queue.append(key)
         return self.cache_data[key]
