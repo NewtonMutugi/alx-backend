e9b4f2af08090c17ccc1260f325800b5ed9e08a1
NewtonMutugi <newtonmut197@gmail.com>
2023-12-09 18:03:28 +0300

    6-job_processor.js

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,16 @@
+import { createQueue } from 'kue';
+
+function sendNotification(phoneNumber, message) {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+
+const queue = createQueue();
+
+queue.process('push_notification_code_2', (job, done) => {
+  const { phoneNumber, message } = job.data;
+
+  sendNotification(phoneNumber, message);
+  done();
+});
